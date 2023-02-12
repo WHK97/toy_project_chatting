7145c2e3482dbfb11c1494b40923e080a2c4da11
WHK97 <woo3681@naver.com>
2023-02-12 16:23:20 +0900

    login

--- server.js
+++ server.js
@@ -26,14 +26,30 @@ app.get('/register',function(req,res){
   res.render('register.ejs');
 });
 app.post('/register',function(req,res){
-  db.collection('user').insertOne({name:req.body.username,email:req.body.email,pw:req.body.pw},function(err,result){
+  db.collection('login').insertOne({name:req.body.name,id:req.body.id,pw:req.body.pw},function(err,result){
   });
   res.redirect('/');
 });
 //로그인
-app.post('/login',function(req,res){
-  db.collection('user').findOne({},function(err,result){
+app.post('/login',passport.authenticate('local',{failureRedirect:'/fail'}), function(req,res){
 
-  });
   res.redirect('friends.ejs');
 })
+passport.use(new LocalStrategy({
+  usernameField: 'id',
+  passwordField: 'pw',
+  session: true,
+  passReqToCallback: false,
+}, function (입력한아이디, 입력한비번, done) {
+  //console.log(입력한아이디, 입력한비번);
+  db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+    if (에러) return done(에러)
+
+    if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+    if (입력한비번 == 결과.pw) {
+      return done(null, 결과)
+    } else {
+      return done(null, false, { message: '비번틀렸어요' })
+    }
+  })
+}));
